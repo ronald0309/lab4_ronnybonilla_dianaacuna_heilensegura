cc9e3b00335e5cee9611a75c719ca3276092b2a9
ronald0309 <ronaldba16@gmail.com>
2023-06-28 13:55:43 -0600

    Actualizacon visual

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'generate_url_page.dart';
-import 'show_url_page.dart';
+import 'formulario.dart';
+import 'mostrar_informacion.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'URL Generator',
+      title: 'Generador de URL',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -24,36 +24,59 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('URL Generator'),
+        title: Text('Generador de URL'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FormularioPage(),
+      body: Container(
+        color:
+            Colors.blueGrey, // Cambiar el color del fondo según tu preferencia
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => FormularioPage(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                );
-              },
-              child: Text('Generate URL'),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => InformacionPage(),
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  textStyle: TextStyle(fontSize: 18),
+                  primary: Colors
+                      .blue, // Cambiar el color del botón según tu preferencia
+                ),
+                child: Text('Generar URL'),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => InformacionPage(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                );
-              },
-              child: Text('Show Information'),
-            ),
-          ],
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  textStyle: TextStyle(fontSize: 18),
+                  primary: Colors
+                      .green, // Cambiar el color del botón según tu preferencia
+                ),
+                child: Text('Mostrar información'),
+              ),
+            ],
+          ),
         ),
       ),
     );
